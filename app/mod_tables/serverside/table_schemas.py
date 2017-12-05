3d4733a0f2f429f693821d3f2ccf65dfab485efd
Jason Li <lja@ohsu.edu>
2017-12-04 18:00:40 -0800

    Add files via upload

---
+++ app/mod_tables/serverside/table_schemas.py
@@ -0,0 +1,30 @@
+SERVERSIDE_TABLE_COLUMNS = [
+    {
+        "data_name": "pmid",
+        "column_name": "pmid",
+        "default": "",
+        "order": 1,
+        "searchable": False
+    },
+    {
+        "data_name": "title",
+        "column_name": "title",
+        "default": "",
+        "order": 2,
+        "searchable": False
+    },
+    {
+        "data_name": "year",
+        "column_name": "year",
+        "default": 0,
+        "order": 3,
+        "searchable": False
+    },
+    {
+        "data_name": "authors",
+        "column_name": "authors",
+        "default": 0,
+        "order": 4,
+        "searchable": True
+    }
+]
